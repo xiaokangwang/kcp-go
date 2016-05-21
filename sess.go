220014cad9d873673300b54c5993747422ac836e
xtaci <daniel820313@gmail.com>
2016-05-21 17:07:10 +0800

    shrink again

--- sess.go
+++ sess.go
@@ -433,7 +433,7 @@ type (
 
 // monitor incoming data for all connections of server
 func (l *Listener) monitor() {
-	chPacket := make(chan packet, 4096)
+	chPacket := make(chan packet, 1024)
 	go l.receiver(chPacket)
 	ticker := time.NewTicker(10 * time.Millisecond)
 	defer ticker.Stop()
